bd6947d026a24cb5589f6c2d16b8adb61876f577
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-06-24 23:13:46 +0300

    create weather service

--- services/get_weather.dart
+++ services/get_weather.dart
@@ -1,11 +1,21 @@
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sohag?key=DXJYZ3UB49AZA87BZBFZB94D3
 
+import 'package:dio/dio.dart';
+import 'package:wheather_app/models/weather.dart';
 
+class WeatherService {
+  final Dio dio;
+  final String baseUrl =
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+  final String apiKey = "DXJYZ3UB49AZA87BZBFZB94D3";
+  WeatherService({required this.dio});
 
-class Weather{
-
-  
-  getWeatherForCountry(){
-
+  Future<Weather?> getWeatherForCountry(String cityName) async {
+    try {
+      Response response = await dio.get('$baseUrl/$cityName?key=$apiKey');
+      return Weather.jSon(response.data);
+    } catch (e) {
+      return null;
+    }
   }
 }
